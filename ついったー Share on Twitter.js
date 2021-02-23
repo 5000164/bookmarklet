484f1edca4240a2fdd88a650a221634e65f1ee0d
Hiroshi Sugawara <5000164@users.noreply.github.com>
2021-02-23 20:40:38 +0900

    refactor: Make the file formatted

--- ついったー Share on Twitter.js
+++ ついったー Share on Twitter.js
@@ -1,1 +1,5 @@
-javascript:(function(){window.open("https://twitter.com/share?url="+encodeURIComponent(location.href)+"&text="+encodeURIComponent(document.title))})()
+// Twitter にシェアする
+
+(() => {
+  window.open("https://twitter.com/share?url=" + encodeURIComponent(location.href) + "&text=" + encodeURIComponent(document.title))
+})()
